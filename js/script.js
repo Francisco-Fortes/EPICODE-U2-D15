8500ff8c62d298dd6defa7272dca174961f179ac
Francisco-Fortes <fortesfco@gmail.com>
2022-11-11 17:01:45 +0100

    JS update
    Add movie working
    WIP: PUT

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const inputCategory = document.querySelector("#movie-category");
+
 //If genre is included(genres fetch) then push movie into genre fetch
 
 //else genre is not included on (genres fetch) then create the genre and then do the if statement
@@ -30,12 +32,21 @@ async function onFormSubmit(event) {
   const newMovie = {
     name: document.querySelector("#movie-name").value,
     description: document.querySelector("#movie-description").value,
-    category: document.querySelector("#movie-category").value,
-    imageUrl: document.querySelector("#movie-image").value,
+    category: inputCategory.value,
+    imageUrl: document.querySelector("#movie-img").value,
   };
-  console.log(newMovie);
-}
 
+  console.log(newMovie);
+
+  fetch("https://striveschool-api.herokuapp.com/api/movies", {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjkxYmQ0YmUzZDAwMTU4NDYwMWQiLCJpYXQiOjE2NjgwODYwNDMsImV4cCI6MTY2OTI5NTY0M30.eZMhitUsKsTXuw2pPSDdb8s15TKDisvdmrvS1a_vuKk",
+    },
+    body: JSON.stringify(newMovie),
+  });
+}
 window.onload = async () => {
   console.log("Am I working?");
 };
