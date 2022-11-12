4b6ec3c9b18b1c124453b3100e6fa4f630ad708c
Francisco-Fortes <fortesfco@gmail.com>
2022-11-12 20:59:35 +0100

    HTML & JS update
    PUT method working
    WIP: Carousel

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const inputCategory = document.querySelector("#movie-category");
+const baseUrl = "https://striveschool-api.herokuapp.com/api/movies/";
+// const keyHeaders =
+// const methodPostPut =
+//Create a carrousel for each genre
 
 //If genre is included(genres fetch) then push movie into genre fetch
 
 //else genre is not included on (genres fetch) then create the genre and then do the if statement
 
 //Fetch Genres
-fetch("https://striveschool-api.herokuapp.com/api/movies/", {
+fetch(baseUrl, {
   method: "GET",
   headers: {
     Authorization:
@@ -14,6 +18,27 @@ fetch("https://striveschool-api.herokuapp.com/api/movies/", {
 })
   .then((response) => response.json())
   .then((categories) => console.log(categories));
+
+// const renderMovies = (arrayOfMovies) => {
+//   arrayOfMovies.forEach((movies) => {
+//     console.log("Inside the forEach()");
+//     console.log(movie);
+//     console.log(movie.name);
+//     container.innerHTML += `<div class="card col-sm-6 col-md-4 col-lg-3" style="width: 18rem;">
+//       <img src="${movie.img}" class="card-img-top" alt="${movie.title}+ cover">
+//       <div class="card-body">
+//         <h5 class="card-title">${book.title}</h5>
+//         <span class="card-text">${book.category}</span>
+//         <span class="card-text">${book.price}</span>
+//         <div class="d-flex justify-content-between">
+//     <button type="button" class="btn btn-primary">Add to cart</button>
+//     <button type="button" class="btn btn-secondary">Skip</button>
+//   </div>;
+//         <div>
+//       </div>
+//     </div>`;
+//   });
+// };
 
 //Fetch Movies
 fetch("https://striveschool-api.herokuapp.com/api/movies/horror", {
@@ -38,14 +63,17 @@ async function onFormSubmit(event) {
 
   console.log(newMovie);
 
-  fetch("https://striveschool-api.herokuapp.com/api/movies", {
+  fetch(baseUrl, {
     method: "POST",
     headers: {
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjkxYmQ0YmUzZDAwMTU4NDYwMWQiLCJpYXQiOjE2NjgwODYwNDMsImV4cCI6MTY2OTI5NTY0M30.eZMhitUsKsTXuw2pPSDdb8s15TKDisvdmrvS1a_vuKk",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(newMovie),
   });
+  //     .then((res) => res.json())
+  //     .then((data) => callback(null, data));
 }
 window.onload = async () => {
   console.log("Am I working?");
